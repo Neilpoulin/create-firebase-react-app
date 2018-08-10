f87a56e6b7c57ef77a2626d668ee2e37b4035503
Neil Poulin <neil.j.poulin@gmail.com>
2018-08-10 00:20:30 -0600

    exclude sample env

--- createApp.sh
+++ createApp.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
 if [[ $# -eq 0 ]] ; then
     echo 'You must provide a project directory name'
     exit 0
 fi
 echo Creating new project $1
-rsync -av --progress . ../$1 --exclude node_modules --exclude functions/node_modules --exclude .git --exclude .idea --exclude dist --exclude createApp.sh
+rsync -av --progress . ../$1 --exclude node_modules --exclude functions/node_modules --exclude .git --exclude .idea --exclude dist --exclude createApp.sh --exclude .env.sample
+
+cp ./.env.sample ../$1/.env
+cp ./.env.sample ../$1/.env.dev
